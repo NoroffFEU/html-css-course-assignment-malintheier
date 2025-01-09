882fb7b6685d326dbaad1eda1a9f872032e48d79
malintheier <malhei01980@stud.noroff.no>
2025-01-09 21:23:14 +0100

    .

---
+++ js/script.js
@@ -0,0 +1,110 @@
+const API_BASE_URL = 'https://v2.api.noroff.dev';
+	
+	async function registerUser(url, data) {
+	  try {
+	    const postData = {
+	      method: 'POST',
+	      headers: {
+	        'Content-Type': 'application/json',
+	      },
+	      body: JSON.stringify(data),
+	    };
+	
+	    const response = await fetch(url, postData);
+	    console.log(response);
+	    const json = await response.json();
+	    console.log(json);
+	    return json;
+	  } catch (error) {
+	    console.log(error);
+	  }
+	}
+	
+	const user = {
+	  name: 'malintheier',
+	  email: 'malhei01980@stud.noroff.no',
+	  password: 'Noroff2002',
+	};
+	
+	registerUser(`${API_BASE_URL}/auth/register`, user);
+
+
+
+    const userLogin = {
+        email: 'malhei01980@stud.noroff.no',
+        password: 'Noroff2002',
+      };
+      
+      async function loginUser(url, data) {
+        try {
+          const postData = {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+          };
+          const response = await fetch(url, postData);
+          console.log(response);
+          const json = await response.json();
+          const accessToken = json.accessToken;
+          localStorage.setItem('accessToken', accessToken);
+          console.log(json);
+          return json;
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      
+      loginUser(`${API_BASE_URL}/auth/login`, user);
+
+
+
+
+
+
+
+// Hentet produkter fra API
+
+const apiUrl = 'https://v2.api.noroff.dev/rainy-days';
+const apiKey = '4af5f64c-f709-4782-8d59-c5436d4ced6b';
+
+// Fetch data from the API
+fetch(apiUrl, {
+  method: 'GET',
+  headers: {
+    'Authorization': `Bearer ${apiKey}`,
+    'Content-Type': 'application/json',
+  },
+})
+
+.then(response => response.json())
+.then(res => {
+    const data =res.data;
+    let products = '';
+    data.forEach(product => {
+        products += `<div class="item">
+        <img src="${product.image.url}" alt="${product.title}"> 
+        <h2>${product.title}</h2> 
+        <p>${product.price} $</p>
+        </div>`
+    })
+    console.log(products);
+    document.getElementById('products-container').innerHTML = products;
+})
+      
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
